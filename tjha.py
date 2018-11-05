42615880456551221a4b4ed958c69eec273b00b3
tjha <tjha@umich.edu>
2018-11-05 14:47:10 -0500

    Q1 done

--- tjha.py
+++ tjha.py
@@ -17,6 +17,7 @@ import copy
 import mytaxi
 import math
 import matplotlib.pyplot as plt
+import trueStateValue as tsv
 
 # Environment for Taxi-v4
 ENV4 = gym.make('Taxi-v4').unwrapped
@@ -47,27 +48,58 @@ def ep_greedy(Q,s,ep):
 # Function implementation of dynaq to handle the stochastic nature of the environment
 # returns Q and cum_steps - list of the cumulatative number of steps counted from the 
 # first episode to the end of each episode.
-def dynaq(env,n=10,gamma=1,alpha=1,epsilon=0.1,episodes=100):
-    # Update kept on cumulative number of steps
-    cum_steps = []
-    Q = np.zeros((env.nS,env.nA))
-    # Using deterministic model
-    # Model = {}
-    # for s in range(env.nS):
-    #     Model[s] = ()
-    # print(Model)
-    #Model = np.zeros((env.nS,env.nA))
-    # Loop over episodes
-    #for i_episode in range(episodes):
-
+def dynaq(env,n=10,gamma=1,alpha=1,epsilon=0.1,runs=1,episodes=100):
+    for run in range(runs):
+        # Update kept on cumulative number of steps
+        cum_steps = np.zeros(episodes)
+        Q = np.zeros((env.nS,env.nA))
+        # Using deterministic model
+        Model = {}
+        for s in range(env.nS):
+            Model[s] = {}
+            for a in range(env.nA):
+                Model[s][a] = (-1, s)
+        # Loop over episodes
+        for idx in range(episodes):
+            visited_states = []
+            taken_actions = {}
+            s = env.reset()
+            visited_states.append(s)
+            done = False
+            counter = 0
+            while not done:
+                a = ep_greedy(Q,s,epsilon)
+                if s in taken_actions:
+                    if a not in taken_actions[s]:
+                        taken_actions[s].append(a)
+                else:
+                    taken_actions[s] = []
+                    taken_actions[s].append(a)
+                ss, r, done, _ = env.step(a)
+                Q[s,a] = Q[s,a] + alpha * (r + gamma * np.max(Q[ss]) - Q[s,a])
+                Model[s][a] = (r, ss)
+                s = ss
+                for i in range(n):
+                    rand_s = np.random.choice(visited_states, size=1)
+                    rand_s = rand_s[0]
+                    rand_a = np.random.choice(taken_actions[rand_s], size=1)
+                    rand_a = rand_a[0]
+                    tup = Model[rand_s][rand_a]
+                    Q[rand_s,rand_a] = Q[rand_s,rand_a] + alpha * (tup[0] + gamma * np.max(Q[tup[1]]) - Q[rand_s,rand_a])
+                counter += 1
+                visited_states.append(s)
+            if idx == 0:
+                cum_steps[idx] = counter
+            else:
+                cum_steps[idx] = counter + cum_steps[idx - 1]
     return Q, cum_steps
 
 # Part (a) - adaptation of qlearn from hw2 to compare with Tabular Dyna-Q
 # Key difference is 100 episodes instead of 500 by default
 # Also, steps are now everaged through multiple callings of function
 def qlearn(env,gamma=1,alpha=0.9,ep=0.05,runs=1,episodes=100):
-    np.random.seed(3)
-    env.seed(5)
+    #np.random.seed(3)
+    #env.seed(5)
     nS = env.nS
     nA = env.nA
     rew_alloc = []
@@ -96,6 +128,105 @@ def qlearn(env,gamma=1,alpha=0.9,ep=0.05,runs=1,episodes=100):
     rew_list = np.mean(np.array(rew_alloc),axis=0)
     return Q, cum_steps
 
+# Modified versions of the algorithms above for usage in random change to v5 environment after 100 episodes
+def original_dynaq(env1, env2, n=10,gamma=1,alpha=1,epsilon=0.1,runs=1,episodes=300):
+    for run in range(runs):
+        # Update kept on cumulative number of steps
+        cum_steps = np.zeros(episodes)
+        Q = np.zeros((env1.nS,env1.nA))
+        # Using deterministic model
+        Model = {}
+        for s in range(env1.nS):
+            Model[s] = {}
+            for a in range(env1.nA):
+                Model[s][a] = (-1, s)
+        env = env1
+        # Loop over episodes
+        for idx in range(episodes):
+            if idx == 100:
+                env = env2
+            visited_states = []
+            taken_actions = {}
+            s = env.reset()
+            visited_states.append(s)
+            done = False
+            counter = 0
+            while not done:
+                a = ep_greedy(Q,s,epsilon)
+                if s in taken_actions:
+                    if a not in taken_actions[s]:
+                        taken_actions[s].append(a)
+                else:
+                    taken_actions[s] = []
+                    taken_actions[s].append(a)
+                ss, r, done, _ = env.step(a)
+                Q[s,a] = Q[s,a] + alpha * (r + gamma * np.max(Q[ss]) - Q[s,a])
+                Model[s][a] = (r, ss)
+                s = ss
+                for i in range(n):
+                    rand_s = np.random.choice(visited_states, size=1)
+                    rand_s = rand_s[0]
+                    rand_a = np.random.choice(taken_actions[rand_s], size=1)
+                    rand_a = rand_a[0]
+                    tup = Model[rand_s][rand_a]
+                    Q[rand_s,rand_a] = Q[rand_s,rand_a] + alpha * (tup[0] + gamma * np.max(Q[tup[1]]) - Q[rand_s,rand_a])
+                counter += 1
+                visited_states.append(s)
+            if idx == 0:
+                cum_steps[idx] = counter
+            else:
+                cum_steps[idx] = counter + cum_steps[idx - 1]
+    return Q, cum_steps
+
+# Modified improvement for dynaq to account for environment change
+def modified_dynaq(env1, env2, n=10,gamma=0.5,alpha=1,epsilon=0.1,runs=1,episodes=300):
+    for run in range(runs):
+        # Update kept on cumulative number of steps
+        cum_steps = np.zeros(episodes)
+        Q = np.zeros((env1.nS,env1.nA))
+        # Using deterministic model
+        Model = {}
+        for s in range(env1.nS):
+            Model[s] = {}
+            for a in range(env1.nA):
+                Model[s][a] = (-1, s)
+        env = env1
+        # Loop over episodes
+        for idx in range(episodes):
+            if idx == 100:
+                env = env2
+            visited_states = []
+            taken_actions = {}
+            s = env.reset()
+            visited_states.append(s)
+            done = False
+            counter = 0
+            while not done:
+                a = ep_greedy(Q,s,epsilon)
+                if s in taken_actions:
+                    if a not in taken_actions[s]:
+                        taken_actions[s].append(a)
+                else:
+                    taken_actions[s] = []
+                    taken_actions[s].append(a)
+                ss, r, done, _ = env.step(a)
+                Q[s,a] = Q[s,a] + alpha * (r + gamma * np.max(Q[ss]) - Q[s,a])
+                Model[s][a] = (r, ss)
+                s = ss
+                for i in range(n):
+                    rand_s = np.random.choice(visited_states, size=1)
+                    rand_s = rand_s[0]
+                    rand_a = np.random.choice(taken_actions[rand_s], size=1)
+                    rand_a = rand_a[0]
+                    tup = Model[rand_s][rand_a]
+                    Q[rand_s,rand_a] = Q[rand_s,rand_a] + alpha * (tup[0] + gamma * np.max(Q[tup[1]]) - Q[rand_s,rand_a])
+                counter += 1
+                visited_states.append(s)
+            if idx == 0:
+                cum_steps[idx] = counter
+            else:
+                cum_steps[idx] = counter + cum_steps[idx - 1]
+    return Q, cum_steps
 
 if __name__ == '__main__':
 
@@ -104,23 +235,63 @@ if __name__ == '__main__':
     # Average results over 20 runs
     #dynaq_Q_avg = 0
     #qlearn_Q_avg = 0
-    dynaq_cum_steps_avg = np.zeros(shape=(100,))
-    qlearn_cum_steps_avg = np.zeros(shape=(100,))
 
-    for i in range(20):
-        print("Performing run: " + str(i + 1))
-        # Randomize seeds so runs are independent
-        np.random.seed(i)
-        ENV4.seed(i)
-        _, dynaq_cum_steps = dynaq(ENV4)
-        _, qlearn_cum_steps = qlearn(ENV4)
-        # Update averages
-        #dynaq_Q_avg += dynaq_Q / 20.0
-        #qlearn_Q_avg += qlearn_Q / 20.0
-        #dynaq_cum_steps_avg += np.divide(dynaq_cum_steps, 20.0)
-        qlearn_cum_steps_avg += np.divide(qlearn_cum_steps, 20.0)
-        break
+    # dynaq_cum_steps_avg = np.zeros(shape=(100,))
+    # qlearn_cum_steps_avg = np.zeros(shape=(100,))
 
-    # Compare results with Q learning implementation used in hw2 in plot
-    print(qlearn_cum_steps_avg)
-    
+    # for i in range(20):
+    #     print("Performing run: " + str(i + 1))
+    #     # Randomize seeds so runs are independent
+    #     np.random.seed(i)
+    #     ENV4.seed(i)
+    #     _, dynaq_cum_steps = dynaq(ENV4)
+    #     _, qlearn_cum_steps = qlearn(ENV4)
+    #     # Update averages
+    #     #dynaq_Q_avg += dynaq_Q / 20.0
+    #     #qlearn_Q_avg += qlearn_Q / 20.0
+    #     dynaq_cum_steps_avg += np.divide(dynaq_cum_steps, 20.0)
+    #     qlearn_cum_steps_avg += np.divide(qlearn_cum_steps, 20.0)
+
+    # # Compare results with Q learning implementation used in hw2 in plot
+    # # Generate plots for Question 1 Part(a)
+    # episodes = np.arange(len(qlearn_cum_steps_avg))
+    # plt.plot(episodes,qlearn_cum_steps_avg, 'r')
+    # plt.plot(episodes, dynaq_cum_steps_avg, 'b')
+    # plt.xlabel("episodes", fontdict={'fontname':'DejaVu Sans', 'size':'20'})
+    # plt.ylabel("cum_steps", fontdict={'fontname':'DejaVu Sans', 'size':'20'})
+    # plt.title("Dynaq and Q-learn cum_steps over episodes", fontdict={'fontname':'DejaVu Sans', 'size':'20'})
+    # plt.savefig("Figure1")
+
+
+    # dynaq_cum_steps_avg1 = np.zeros(shape=(300,))
+    # dynaq_cum_steps_avg2 = np.zeros(shape=(300,))
+    # for i in range(5):
+    #     print("Performing run: " + str(i + 1))
+    #     # Randomize seeds so runs are independent
+    #     np.random.seed(i)
+    #     ENV4.seed(i)
+    #     ENV5.seed(i)
+    #     _, dynaq_cum_steps1 = original_dynaq(ENV4, ENV5)
+    #     _, dynaq_cum_steps2 = modified_dynaq(ENV4, ENV5)
+    #     # Update averages
+    #     #dynaq_Q_avg += dynaq_Q / 20.0
+    #     #qlearn_Q_avg += qlearn_Q / 20.0
+    #     dynaq_cum_steps_avg1 += np.divide(dynaq_cum_steps1, 5.0)
+    #     dynaq_cum_steps_avg2 += np.divide(dynaq_cum_steps2, 5.0)
+
+    # episodes = np.arange(len(dynaq_cum_steps_avg1))
+    # plt.plot(episodes,dynaq_cum_steps_avg1, 'r')
+    # plt.plot(episodes, dynaq_cum_steps_avg2, 'b')
+    # plt.xlabel("episodes", fontdict={'fontname':'DejaVu Sans', 'size':'20'})
+    # plt.ylabel("cum_steps", fontdict={'fontname':'DejaVu Sans', 'size':'20'})
+    # plt.title("Dynaq and Modified DynaQ cum_steps over episodes", fontdict={'fontname':'DejaVu Sans', 'size':'20'})
+    # plt.savefig("Figure2")
+
+    alpha = np.arange(0,1.1, 0.1)
+    n = np.power(2,np.arange(0,10))
+
+    print(alpha)
+    print(n)
+
+
+
